19a14a0630a8295a49e3e43f14096f49a8fdaa49
Matias <mati.dehoyosmdp@gmail.com>
2024-11-28 18:31:30 +0000

    correct

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -104,9 +104,9 @@ const ProductDetailPage = () => {
               <div className='w-full flex flex-col md:items-center py-12 md:py-20 bg-[#1b7b7e] md:bg-gray-400  gap-6 md:gap-10'>
                 <h3 className='pl-[3%] md:pl-0 text-2xl font-normal md:font-bold text-gray-50 md:text-gray-800'>También te puede interesar</h3>
                 <div className='w-full px-[3%] flex md:flex-wrap justify-start md:justify-center gap-3 md:gap-5 overflow-scroll' style={{ scrollbarWidth: 'none' }}>
-                 { contenido.map(libro => (
-                  <RelatedCard key={libro.id} libro={libro} />
-                 ))}
+                    { contenido.map(libro => (
+                           <RelatedCard  libro={libro} />
+                    ))}
               </div>
             </div>
     </div>
